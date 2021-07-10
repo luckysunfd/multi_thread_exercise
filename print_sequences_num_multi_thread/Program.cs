5aadd87821c4098ad0f65213f5463f576d5bf8f6
luckysunfd <lingganyang@gmail.com>
2021-07-10 20:24:35 +0800

    n个线程按照数字顺序打印
    假设三个线程，交替顺序打印0-100之间的数字
    
    1. 线程a 打印完
    2. 线程b 接着打印
    3. 线程c 紧跟线程b之后打印
    
    不断重复上面的步骤，直到打印到100
    
    通过一个静态的全局变量cur-num记录要打印的数字

---
+++ print_sequences_num_multi_thread/Program.cs
@@ -0,0 +1,72 @@
+﻿// 多个线程，打印0-100，按照顺序打印
+// 假设有五个线程，交替执行，a，b，c，d，e
+// 各个线程要打印的数字通过一个static int记录
+
+using System;
+using System.Threading;
+
+
+namespace print_sequences_num_multi_thread
+{
+    class Program
+    {
+        // 当前要打印的数字
+        static int cur = 0;
+        // 三个线程，三个信号量
+        static Semaphore s1 = new Semaphore(1, 1);
+        static Semaphore s2 = new Semaphore(0, 1);
+        static Semaphore s3 = new Semaphore(0, 1);
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(f1));
+            Thread t2 = new Thread(new ThreadStart(f2));
+            Thread t3 = new Thread(new ThreadStart(f3));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static void f1()
+        {
+            while(cur <= 100)
+            {
+                s1.WaitOne();
+                if(cur <= 100)
+                    Console.WriteLine("{0} - tId:{1}",cur, Thread.CurrentThread.ManagedThreadId);
+                
+                cur++;
+                s2.Release();
+            }
+        }
+        static void f2()
+        {
+            while(cur <= 100)
+            {
+                s2.WaitOne();
+                if(cur <= 100)
+                    Console.WriteLine("{0} - tid:{1}", cur, Thread.CurrentThread.ManagedThreadId);
+                cur++;
+                s3.Release();
+            }
+        }
+        static void f3()
+        {
+            while(cur <= 100)
+            {
+                s3.WaitOne();
+                if(cur <= 100)
+                    Console.WriteLine("{0} - tid:{1}", cur, Thread.CurrentThread.ManagedThreadId);
+                cur++;
+                s1.Release();
+            }
+        }
+    }
+}
