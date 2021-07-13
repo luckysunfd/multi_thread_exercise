df59e326aaf2ddd918906c0711c533deefd04c33
luckysunfd <lingganyang@gmail.com>
2021-07-13 20:58:19 +0800

    使用信号量、manualresetevent 实现的两个线程交替打印奇偶数

---
+++ odd_even5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+// 使用信号量 实现 交替打印 奇偶数 两个线程 
+
+namespace odd_even5
+{
+    class Program
+    {
+        private static Semaphore s1 = new Semaphore(0, 1);
+        private static Semaphore s2 = new Semaphore(1, 1);
+
+        private static void odd()
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                if( i % 2 == 0 )
+                {
+                    s1.WaitOne();
+                    Console.WriteLine(i);
+                }else{
+                    s2.Release();
+                }
+            }
+        }
+
+        private static void even()
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                if( i % 2 == 1)
+                {
+                    s2.WaitOne();
+                    Console.WriteLine(i);
+                }else{
+                    s1.Release();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(odd));
+            Thread t2 = new Thread(new ThreadStart(even));
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ odd_evenv4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+// 两个线程， 交替执行，线程a先输出偶数，线程b紧跟着输出奇数，循环往复，直到到达终止点
+
+namespace odd_evenv4
+{
+    class Program
+    {
+        private static ManualResetEvent mreodd = new ManualResetEvent(false);
+        private static ManualResetEvent mreeven = new ManualResetEvent(true);
+
+        private static void odd()
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                if(i % 2 == 0)
+                {   mreodd.WaitOne();
+                    Console.WriteLine("{0} - {1}", Thread.CurrentThread.ManagedThreadId, i);
+                    mreodd.Reset();
+                }else{
+                    mreeven.Set();
+                }
+            }
+        }
+
+        private static void even()
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                if( i % 2 == 1)
+                {
+                    mreeven.WaitOne();
+                    Console.WriteLine("{0} - {1}", Thread.CurrentThread.ManagedThreadId ,i);
+                    mreeven.Reset();
+                }else{
+                    mreodd.Set();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(odd));
+            Thread t2 = new Thread(new ThreadStart(even));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
